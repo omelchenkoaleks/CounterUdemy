b003e5821ee7e2d7261fb8c56536eb77e338602c
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-29 17:51:06 +0300

    Добавляем LiveData для хранения данных. Чтобы не вручную обрабатывать данные.

--- app/src/main/java/com/omelchenkoaleks/counterudemy/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/counterudemy/MainActivity.kt
@@ -3,25 +3,32 @@ package com.omelchenkoaleks.counterudemy
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
+    private lateinit var liveData: MutableLiveData<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        textView.text = mainViewModel.getCurrentValue().toString()
+
+        liveData = mainViewModel.getCurrentValue()
+        liveData.observe(this, Observer {
+            textView.text = it.toString()
+        })
     }
 
     fun decreaseValue(view: View) {
-        textView.text = mainViewModel.getDecreaseValue().toString()
+        mainViewModel.getDecreaseValue().toString()
     }
 
     fun increaseValue(view: View) {
-        textView.text = mainViewModel.getIncreaseValue().toString()
+        mainViewModel.getIncreaseValue().toString()
     }
 }
--- app/src/main/java/com/omelchenkoaleks/counterudemy/MainViewModel.kt
+++ app/src/main/java/com/omelchenkoaleks/counterudemy/MainViewModel.kt
@@ -1,19 +1,24 @@
 package com.omelchenkoaleks.counterudemy
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     var counter = 1
+    private val liveData = MutableLiveData<Int>()
 
-    fun getCurrentValue(): Int {
-        return counter
+    fun getCurrentValue(): MutableLiveData<Int> {
+        liveData.value = counter
+        return liveData
     }
 
-    fun getDecreaseValue(): Int {
-        return --counter
+    fun getDecreaseValue() {
+        --counter
+        liveData.value = counter
     }
 
-    fun getIncreaseValue(): Int {
-        return ++counter
+    fun getIncreaseValue() {
+        ++counter
+        liveData.value = counter
     }
- }
+}
